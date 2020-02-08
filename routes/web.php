c729f47b09a821d2e32097a66258f6fd3d23ea07
berrlove0147 <feeltheglow555@gmail.com>
2020-02-08 15:08:12 +0700

    วีดีโอ

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,55 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/test-video', function () {
-    return view('partials.url-video-player');
+    return view('partials.vimeo-video-player');
 });
+
+//แสดงรายการ ซีรีย์/ตอน index
+route::get('/series', function () {
+    $series =\App\Serie::all();
+    return view('serie.index')->with([
+        'series' => $series
+        ]);
+
+})->name('series');
+
+//แสดงฟอร์มสร้าง ซีรีย์/ตอน
+route::get('/series/create', function () {
+    return view('serie.create');
+});
+
+route::get('/series/{serieId}/episodes/create', function ($serieId) {
+    return view('episode.create')->with(['serieId' =>$serieId]);
+});
+
+//รับข้อมูลจากฟอร์มสร้าง ซีรีย์/ตอน แล้วบันทึกลงตาราง
+route::post('/series/{id}/episodes', function ($id) {
+    $data= \Request::all();
+
+    $episode = \App\Episode::create($data);
+
+    $episode->serie_id = $id;
+
+    $episode->save();
+
+    return redirect('/series/'.$id);
+});
+
+//แสดง ตอน ที่มีอยู่ในซีรีย์
+// route::get('/series/{id}',function($id) {
+//     $serie = \App\Serie::find($id);
+
+//     return view('serie.show')->with([
+//         'serie'=> $serie
+//     ]);
+// });
+
+route::get('/series/{serie}',function(\App\Serie $serie) {
+    // $serie = \App\Serie::find($id);
+
+    return view('serie.show')->with([
+        'serie'=> $serie
+    ]);
+});
+
 
